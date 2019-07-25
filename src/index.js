2713515a30b81a48e8879b10f628402039d4b4f8
Eugene Kopich <github@web2033.com>
2019-07-25 14:48:11 +0300

    intersection-observer polyfill

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
+require('intersection-observer');
 import './main.scss';
-
 import trigger from '@web2033/trigger';
 import anime from 'animejs';
-
-/* Triggers to lazy-load images */
-
-/* trigger({
-  targets: 'img',
-  margin: '20%',
-  action(node) {
-    node.src = node.dataset.src;
-  }
-}); */
